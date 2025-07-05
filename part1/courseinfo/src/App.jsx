38647ccc15edbaa3299d6eb11196d61d9f53e0e5
Vaibhav Mane <vmane9357@gmail.com>
2025-07-05 17:19:16 +0530

    Add Unicafe app for exercises 1.6–1.11

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,13 +1,11 @@
-const Header = ({ course }) => {
-  return (
-    <h1>{course}</h1>
-  )
+// App.jsx
+
+const Header = ({ courseName }) => {
+  return <h1>{courseName}</h1>
 }
 
 const Part = ({ name, exercises }) => {
-  return (
-    <p>{name} {exercises}</p>
-  )
+  return <p>{name} {exercises}</p>
 }
 
 const Content = ({ parts }) => {
@@ -22,24 +20,33 @@ const Content = ({ parts }) => {
 
 const Total = ({ parts }) => {
   const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
-  return (
-    <p>Number of exercises {total}</p>
-  )
+  return <p>Number of exercises {total}</p>
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
